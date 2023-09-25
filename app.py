cb173691e38e720259cc46816c0b936e91cc8beb
Jose Armas <armasjose95@gmail.com>
2023-09-24 23:57:03 -0500

    extracting some text

--- app.py
+++ app.py
@@ -20,7 +20,21 @@ print(soup.prettify())
 # a ',' comma separates each 'div' class
 
 
-soup.find_all('div', class_='col-md-12')  # returns just this information
+# soup.find_all('div', class_='col-md-12')  # returns just this information
 # you can return multiple if there is multiple
 
-soup.find_all('p')  # returns just this 'p' information
+# soup.find_all('p')  # returns any 'p' information
+
+
+# soup.find_all('p', class_= 'lead') # find 'p' div w/ specific lead class
+
+# .text
+# trying to pull some info or a paragraph of text for ex.
+# soup.find_all('p', class_= 'lead').text # error
+# error because i'm using find all and find all doesn't have a text attribute
+# can switch to just find
+# use find_all most of the time unless you want to extract text
+
+
+soup.find('p', class_='lead').text.strip()
+# pulls text between quotes no longer in a list or with 'p' tags for ex. or class attribute
