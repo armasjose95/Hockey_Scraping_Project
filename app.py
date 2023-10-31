5b69c803bbd7e53237aa8dfb215d82443bbe36ac
Jose Armas <armasjose95@gmail.com>
2023-10-30 22:38:40 -0500

    finding table where data lives

--- app.py
+++ app.py
@@ -5,36 +5,27 @@ url = 'http://www.scrapethissite.com/pages/forms/'
 
 page = requests.get(url)
 
+
 # <Response [200]>
 
 soup = BeautifulSoup(page.text, 'html')
 # page is sending the request & html is retrieving raw html I am using
 # html will parse the info in an html format
+print(soup)
 
-print(soup.prettify())
-
-# soup.find('div') # this finds the first 'div' class
-
-
-# soup.find_all('div') # finds all 'div' on the page
-# a ',' comma separates each 'div' class
+soup.find('table')
 
 
-# soup.find_all('div', class_='col-md-12')  # returns just this information
-# you can return multiple if there is multiple
-
-# soup.find_all('p')  # returns any 'p' information
+# soup.find('th').text.strip()
+# = 'Team Name'
 
 
-# soup.find_all('p', class_= 'lead') # find 'p' div w/ specific lead class
-
-# .text
-# trying to pull some info or a paragraph of text for ex.
-# soup.find_all('p', class_= 'lead').text # error
-# error because i'm using find all and find all doesn't have a text attribute
-# can switch to just find
-# use find_all most of the time unless you want to extract text
+# soup.find_all('td', class_='name')
 
 
-soup.find('p', class_='lead').text.strip()
-# pulls text between quotes no longer in a list or with 'p' tags for ex. or class attribute
+# table = soup.find_all('table')
+# print(table)
+
+
+# column_titles = table('th')
+# column_titles
