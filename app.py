eae904a899103e4c307e0e39b624f68636789576
Jose Armas <armasjose95@gmail.com>
2023-10-31 22:23:13 -0500

    created & printed column titles for my table

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 url = 'http://www.scrapethissite.com/pages/forms/'
 
@@ -8,7 +9,7 @@ page = requests.get(url)
 
 # <Response [200]>
 
-soup = BeautifulSoup(page.text, 'html')
+soup = BeautifulSoup(page.text, features="html.parser")
 # page is sending the request & html is retrieving raw html I am using
 # html will parse the info in an html format
 print(soup)
@@ -16,16 +17,27 @@ print(soup)
 soup.find('table')
 
 
+soup.find('table', class_='table')
+
+
+table = soup.find('table')
+print(table)
+
+
 # soup.find('th').text.strip()
 # = 'Team Name'
 
 
-# soup.find_all('td', class_='name')
+column_titles = table('th')
+print(column_titles)
+
+# looping through each column header/title[th tag] so I can loop trough to get each header/title
+hockey_column_titles = [title.text.strip() for title in column_titles]
+print(hockey_column_titles)
+# ['Team Name', 'Year', 'Wins', 'Losses', 'OT Losses', 'Win %', 'Goals For (GF)', 'Goals Against (GA)', '+ / -']
 
 
-# table = soup.find_all('table')
-# print(table)
+# enter titles into pandas data frame
 
-
-# column_titles = table('th')
-# column_titles
+df = pd.DataFrame(columns=hockey_column_titles)
+df
