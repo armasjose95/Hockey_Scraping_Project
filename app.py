4a308880870581ea5d228075fb293ca725745e38
Jose Armas <armasjose95@gmail.com>
2023-11-04 18:52:08 -0500

    addinh table info through a for loop

--- app.py
+++ app.py
@@ -38,6 +38,29 @@ print(hockey_column_titles)
 
 
 # enter titles into pandas data frame
-
+# imported pandas
+# headers are now printed
 df = pd.DataFrame(columns=hockey_column_titles)
+df
+
+
+# start pulling in data for each column
+# they come in with their headers, so will need to get rid of them
+column_data = table.find_all('tr')
+
+
+# need to loop through because this is a list
+# as it gets looped through I'm finding all and looking for the td tags(individual data(row data))
+# then I'm taking each piece of data & getting out the text & stripping it to clean it
+# now in a list for each individual row
+for row in column_data:
+    row_data = row.find_all('td')
+    individual_row_data = [data.text.strip() for data in row_data]
+
+
+# then take the length and use it when I put in this new info
+# checking length of our data frame each time it's looping through
+# and then I'm going to put the info in the next position by appending it to our empty data frame
+length = len(df)
+df.loc[length] = individual_row_data
 df
